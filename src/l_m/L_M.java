89dbf12c4cd8b8c85cbf677f509e8129b75d3b07
pedro <pedro28113316@gmail.com>
2025-02-28 15:10:30 -0300

    primeiro

---
+++ src/l_m/L_M.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l_m;
+
+/**
+ *
+ * @author Pedro53722376
+ */
+public class L_M {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
